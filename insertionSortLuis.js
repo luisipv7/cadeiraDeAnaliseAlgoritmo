00e5d553385e75708e5f128d2ff32198eaa211ff
Luis Fernando Flores dos Santos <luis.santos@govbr.com.br>
2020-09-18 16:57:49 -0300

    AtualizandoNomenclatura

--- insertionSortLuis.js
+++ insertionSortLuis.js
@@ -6,13 +6,13 @@ vetor.push(Number(newValue))
 
 console.log(vetor);
 
-for (let ant = 1; ant < vetor.length; ant++) {
-  let prox = ant - 1
-  atual = vetor[ant]
-  for (prox >= 0; atual < vetor[prox]; prox--) {
-    vetor[prox + 1] = vetor[prox]
+for (let prox = 1; prox < vetor.length; prox++) {
+  let ant = prox - 1
+  atual = vetor[prox]
+  for (ant >= 0; atual < vetor[ant]; ant--) {
+    vetor[ant + 1] = vetor[ant]
   }
-  vetor[prox + 1] = atual
+  vetor[ant + 1] = atual
 }
 
 console.log(vetor)
--- insertionSortLuis.py
+++ insertionSortLuis.py
@@ -2,13 +2,13 @@ vetor = [20, 8, 99, 800, 9, 1, 801]
 
 atual = []
 
-for ant in range(len(vetor)):
-    # print(vetor[ant])
-    prox = ant - 1
-    atual = vetor[ant]
-    while prox >= 0 and atual < vetor[prox]:
-        vetor[prox+1] = vetor[prox]
-        prox -= 1
-    vetor[prox + 1] = atual
+for prox in range(len(vetor)):
+    # print(vetor[prox])
+    ant = prox - 1
+    atual = vetor[prox]
+    while ant >= 0 and atual < vetor[ant]:
+        vetor[ant+1] = vetor[ant]
+        ant -= 1
+    vetor[ant + 1] = atual
 
 print(vetor)
